f70f72ec01a8f880ddd9d27ba2b1b733dfa9228f
brev1ch <flirktv@gmail.com>
2024-11-24 23:56:11 +0300

    Triangle

--- triangle.cpp
+++ triangle.cpp
@@ -8,70 +8,57 @@ double Triangle::side_length(point a, point b)
   return std::sqrt(std::pow(length_x, 2.0) + std::pow(length_y, 2.0));
 }
 
-double Triangle::perimetr(double lena_, double lenb_, double lenc_)
+double Triangle::find_perimetr(double lena_, double lenb_, double lenc_)
 {
   return lena_ + lenb_ + lenc_;
 }
 
-double Triangle::square(double lena_, double lenb_, double lenc_, double perimetr_)
+double Triangle::find_area(double lena_, double lenb_, double lenc_, double perimetr_)
 {
   double p = perimetr_ / 2.0;
   return std::sqrt(p * (p - lena_) * (p - lenb_) * (p - lenc_));
 }
 
-bool brevnov::checkTable(double lena_, double lenb_, double lenc_)
+bool Triangle::checkTable(double lena_, double lenb_, double lenc_)
 {
   return (lena_ >= lenb_ + lenc_) || (lenb_ >= lena_ + lenc_) || (lenc_ >= lenb_ + lena_);
 }
 
-void brevnov::sort_ascending_order(Triangle & e)
+void Triangle::sort_ascending_order(Triangle & e)
 {
-  double * a = &e.lena_;
-  double * b = &e.lenb_;
-  double * c = &e.lenc_;
-  double * help = nullptr;
-  if (a < b)
-  {
-    if (a < c)
-    {
-      if (b > c)
-      {
-        help = b;
-        b = c;
-        c = help;
-      }
-    }
-    else
-    {
-      help = a;
-      a = c;
-      c = b;
-      b = help;
-    }
-  }
-  else
-  {
-    if (a < c)
-    {
-      help = a;
-      a = b;
-      b = help;
-    }
-    else
-    {
-      if (c > b)
-      {
-        help = a;
-        a = b;
-        b = c;
-        c = help;
-      }
-      else
-      {
-        help = a;
-        a = c;
-        c = help;
-      }
-    }
-  }
+  double a = 0.0, b = 0.0, c = 0.0;
+  a = std::max(e.lena_, e.lenb_, e.lenc_);
+  c = std::max(e.lena_, e.lenb_, e.lenc_);
+  b = std::sum(e.lena_, e.lenb_, e.lenc_) - a - c;
+  e.lena_ = a;
+  e.lenb_ = b;
+  e.lenc_ = c;
+}
+
+std::istream& operator >> (std::istream& in, Triangle& e)
+{
+  in >> e.a_.x >> e.a_.y >> e.b_.x >> e.b_.y >> e.c_.x >> e.c_.y;
+  e.lena_ = Triangle::side_length(e.b_, e.c_);
+  e.lenb_ = Triangle::side_length(e.a_, e.c_);
+  e.lenc_ = Triangle::side_length(e.a_, e.b_);
+  e.perimetr_ = Triangle::find_perimetr(e.lena_, e.lenb_, e.lenc_);
+  e.area_ = Triangle::find_area(e.lena_, e.lenb_, e.lenc_, e.perimetr_);
+  return in;
+}
+bool operator == (Triangle& a, Triangle& b)
+{
+  return (a.lena_ == b.lena_) && (a.lenb_ == b.lenb_) && (a.lenc_ == b.lenc_);
+}
+bool operator < (Triangle& a, Triangle& b)
+{
+  return a.area_ < b.area_;
+}
+void operator += (Triangle& e, double k)
+{
+  e.a_.x += k;
+  e.b_.x += k;
+  e.c_.x += k;
+  e.a_.y += k;
+  e.b_.y += k;
+  e.c_.y += k;
 }
--- triangle.hpp
+++ triangle.hpp
@@ -9,70 +9,22 @@ struct point
 class Triangle
 {
 public:
-  Triangle() 
-  {
-    a_.x = 0;
-    a_.y = 0;
-    b_.x = 0;
-    b_.y = 0;
-    c_.x = 0;
-    c_.y = 0;
-    lena_ = 0;
-    lenb_ = 0;
-    lenc_ = 0;
-    perimetr_ = 0;
-    square_ = 0;
-  }
-  Triangle(point a, point b, point c)
-  {
-    a_ = a;
-    b_ = b;
-    c_ = c;
-    lena_ = side_length(b_, c_);
-    lenb_ = side_length(a_, c_);
-    lenc_ = side_length(a_, b_);
-    perimetr_ = perimetr(lena_, lenb_, lenc_);
-    square_ = square(lena_, lenb_, lenc_, perimetr_);
-  }
-  point a_, b_, c_;
-  double lena_, lenb_, lenc_;
-  double square_, perimetr_;
-  void calculate_the_triangle(Triangle & a);
+  Triangle(const point& a = {}, const point& b = {}, const point& c = {}) :
+    a_(a), b_(b), c_(c), lena_(side_length(a, b)), lenb_(side_length(b, c)), lenc_(side_length(a, c)), perimetr_(find_perimetr(lena_, lenb_, lenc_)), area_(find_area(double lena_, double lenb_, double lenc_, double perimetr_)) {}
   double side_length(point a, point b);
-  double perimetr(double lena_, double lenb_, double lenc_);
-  double square(double lena_, double lenb_, double lenc_, double perimetr_);
-};
-std::istream& operator >> (std::istream& in, Triangle& e)
-{
-  in >> e.a_.x >> e.a_.y >> e.b_.x >> e.b_.y >> e.c_.x >> e.c_.y;
-  e.lena_ = Triangle::side_length(e.b_, e.c_);
-  e.lenb_ = Triangle::side_length(e.a_, e.c_);
-  e.lenc_ = Triangle::side_length(e.a_, e.b_);
-  e.perimetr_ = Triangle::perimetr(e.lena_, e.lenb_, e.lenc_);
-  e.square_ = Triangle::square(e.lena_, e.lenb_, e.lenc_, e.perimetr_);
-  return in;
-}
-bool operator == (Triangle& a, Triangle& b)
-{
-  return (a.lena_ == b.lena_) && (a.lenb_ == b.lenb_) && (a.lenc_ == b.lenc_);
-}
-bool operator < (Triangle& a, Triangle& b)
-{
-  return a.square_ < b.square_;
-}
-void operator += (Triangle& e, double k)
-{
-  e.a_.x += k;
-  e.b_.x += k;
-  e.c_.x += k;
-  e.a_.y += k;
-  e.b_.y += k;
-  e.c_.y += k;
-}
-namespace brevnov{
+  double find_perimetr(double lena_, double lenb_, double lenc_);
+  double find_area(double lena_, double lenb_, double lenc_, double perimetr_);
   void sort_ascending_order(Triangle & e);
   void moveTriangle(Triangle & e, double k);
   bool checkTable(double lena_, double lenb_, double lenc_);
   bool isEqualAreas(Triangle & a, Triangle & b);
-}
+  friend std::istream& operator >> (std::istream& in, Triangle& e);
+  friend bool operator == (Triangle& a, Triangle& b);
+  friend void operator += (Triangle& e, double k);
+  friend bool operator < (Triangle& a, Triangle& b);
+private:
+  point a_, b_, c_;
+  double lena_, lenb_, lenc_;
+  double area_, perimetr_;
+};
 #endif
